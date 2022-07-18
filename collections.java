12afdb35e93342687e25b78bfd95c5b0b5026215
Marcelo Rodrigues da Silva <marcelo.eng.iar@gmail.com>
2022-07-17 21:48:59 -0300

    Update collections.java

--- collections.java
+++ collections.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class collections {
+public class Collections {
 
 
     public static void main(String[] args) {
